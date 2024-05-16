bbe20ea731c02d662ef92034882ba5a2396ef883
hatatori <kcireh.i@gmail.com>
2024-05-16 02:48:02 -0300

    ok

--- index.js
+++ index.js
@@ -13,54 +13,109 @@ const HTML = {
 // const size = 100
 // const randomness = 10
 
-
-
 const Logic = {
     lin: 5,
     col: 4,
     size: 80,
-    randomness: 7,
+    randomness: 5,
     blocks: [],
-    Fruits: '🍅 🌶️ 🎂 🍉 🥤 🍰 🥝 🍫 🍎 🍏 🍒 🍑 🍌 🍅 🌶️ 🎂 🍰 🍫 🥜 🍦 🍨 🥭'.split(" "),
+    Fruits: '🍅 🌶️ 🎂 🍉 🥤 🍰 🥝 🍫 🍒 🍏 🍑 🍌 🍅 🌶️ 🎂 🍰 🍫 🥜 🍦 🍨 🥭'.split(" "),
     table: [
-        [0, 8, 8, 5, 1],
-        [5, 9, 9, 0, 1],
-        [3, 3, 3, 3, 1],
-        [5, 9, 8, 2, 1],
-        [5, 1, 1, 1, 0],
+        [0, 8, 8, 5],
+        [5, -1, 9, 0],
+        [3, -1, 3, 3],
+        [5, 9, 8, 2],
+        [5, 1, 2, 1],
     ],
+    canPlay: true,
 
-    Falls(){
-        sm = Logic.SubMatrix()
-        Graphics.CheckBin()
-        setTimeout(()=>{
-            for(let r=0;r<10;r++){
-                Logic.Fall()
-            }
-        }, 300)
+    refresh(){
+        if(Logic.randomness > Logic.Fruits.length-1)
+            Logic.randomness = Logic.Fruits.length-1
         
+        Logic.setGrid(Logic.lin, Logic.col)
+        Logic.generate()
     },
 
-    Fall(){
-        console.log(sm)
-        for(i=0;i<Logic.lin;i++){
+    // Falls(){
+
+    //     // sm = Logic.SubMatrix()
+    //     // Graphics.ClearLines()
+        
+    //     for(let r=0;r<10;r++){
+    //         setTimeout(()=>{
+    //             Logic.Fall()
+    //         }, r*50)
+    //     }
+
+    //     // setTimeout(()=>{
+    //     //     for (let j = 0; j < Logic.col; j++) {
+    //     //         if(Logic.table[0][j] == -1){
+    //     //             console.log('ok')
+    //     //             const r = Math.random()*Logic.randomness|0
+    //     //             Logic.table[0][j] = r
+    //     //             Graphics.PieceAdd([0,j], r)
+    //     //         }
+    //     //     }
+    //     // }, 400)
+         
+    // },
+
+    // Fall(){
+    //     sm = Logic.SubMatrix()
+
+    //     for(i=Logic.col;i>0;i--){
+    //         LinA = i-1
+    //         LinB = i
+            
+    //         for(j=0;j<Logic.col;j++){
+    //             col_a = sm[LinA][j]
+    //             col_b = sm[LinB][j]
+                
+    //             if(col_b == -1){
+    //                 Logic.Down([LinB,j],[LinA,j]);
+    //             }
+    //         }
+    //         Logic.table = sm
+    //     }     
+    // },
+
+    Renew(){
+        // add new fruits in first line after clear repeated fruits
+
+        while(Logic.table[0].includes(-1)){
             for(j=0;j<Logic.col;j++){
-                if(sm[i][j] == -1){
-                    try{
-                        if(sm[i][j] == -1){
-                            aux = sm[i][j]
-                            sm[i][j] = sm[i-1][j]
-                            sm[i-1][j] = aux
-                            Logic.Swap([i,j],[i-1,j])
-                        }
-                    }catch(err){}
+                // console.log(Logic.table[0][j])
+                if(Logic.table[0][j] == -1){
+                    Graphics.PieceAdd([0,j])
+                    Logic.Downs()
                 }
             }
         }
-        Logic.table = sm
+
+    },
+
+    Downs(){
+        for (let i = 0; i < Logic.lin; i++) {
+            for (let j = 0; j < Logic.col; j++) {
+                Logic.Down(i,j)
+            }
+        }
+    },
+
+    Down(i, j){
+        try{
+            if(Logic.table[i+1][j] != -1) return
+            aux = Logic.table[i][j]
+            Logic.table[i][j] = Logic.table[i+1][j]
+            Logic.table[i+1][j] = aux
+            el = Graphics.getElementByCoordinate(i, j)
+            el.style.top = (i+1)*Logic.size+'px'
+            el.style.left = j*Logic.size+'px'
+            el.setAttribute('lin', (i+1))
+        }catch(err){}
     },
     
-
     Swap(a,b){
         const t1 = Graphics.getElementByCoordinate(a[0], a[1])
         const t2 = Graphics.getElementByCoordinate(b[0], b[1])
@@ -77,7 +132,7 @@ const Logic = {
         this.table[a[0]][a[1]] = this.table[b[0]][b[1]]
         this.table[b[0]][b[1]] = aux
 
-        // Graphics.CheckBin()
+        // Graphics.ClearLines()
         
         // const e1 = HTML.GameScreen.children[a[0] * this.col + a[1]]
         // const e2 = HTML.GameScreen.children[b[0] * this.col + b[1]]
@@ -90,11 +145,23 @@ const Logic = {
     },
 
     setGrid(lin, col) {
+        HTML.GameScreen.innerHTML = ''
+        HTML.GameScreen.style.width = col*Logic.size+'px'
+        HTML.GameScreen.style.height = lin*Logic.size+'px'
+
+        // const t = []
+        // for (let i = 0; i < lin; i++) {
+        //     t[i] = []
+        //     for (let j = 0; j < col; j++) {
+        //         t[i].push(Math.random() * Logic.randomness | 0)
+        //     }
+        // }
+
         const t = []
         for (let i = 0; i < lin; i++) {
             t[i] = []
             for (let j = 0; j < col; j++) {
-                t[i].push(Math.random() * Logic.randomness | 0)
+                t[i].push(-1)
             }
         }
 
@@ -108,16 +175,51 @@ const Logic = {
     generate() {
         // Logic.createTableRandom(5, 5)
         Graphics.Cenario()
-        Graphics.Pieces()
+
+        for(i=0;i<Logic.lin;i++){
+            for(j=0;j<Logic.col;j++){
+                Graphics.PieceAdd([i,j])
+                // Logic.Downs()
+            }
+        }
+        // Logic.Downs()
+
+
+        // Graphics.Pieces()
     },
+
     generateTable(table){
-        this.table = table
+        Logic.table = table
         Graphics.Cenario()
-        Graphics.Pieces()
+
+        for(i=0;i<Logic.lin;i++){
+            for(j=0;j<Logic.col;j++){
+                if(Logic.table[i][j] != -1)
+                    Graphics.PieceAdd([i,j], Logic.table[i][j])
+                // Logic.Downs()
+            }
+        }
+        // Graphics.Pieces()
+
+        // for(i=0;i<Logic.lin;i++){
+        //     for(j=0;j<Logic.col;j++){
+        //         if(Logic.table[i][j] >= 0)
+        //             Graphics.PieceAdd([i,j])
+        //         // Logic.Downs()
+        //     }
+        // }
+
+        // this.generate()
+
+
+        // Graphics.PieceAdd([0,j])
+
     },
+
     rotateMatrix(matrix) {
         return matrix[0].map((e, i) => matrix.map(row => row[i]));
     },
+
     toBin(x){
         
         // let x = [0, 0, 0, 6, 6, 3, 3, 3, 0, 5, 5, 5, 5, 9, 8];
@@ -147,6 +249,7 @@ const Logic = {
     
         return groups.flat()
     },
+
     toBinMatrix(matrix){
         x = matrix.map(e=> this.toBin(e) )
         return x
@@ -192,45 +295,115 @@ const Graphics = {
         HTML.GameScreen.style.height = Logic.lin * Logic.size + 'px'
     },
 
-    Pieces() {
-        HTML.GameScreen.innerHTML = ''
-        for (let i = 0; i < Logic.lin; i++) {
-            for (let j = 0; j < Logic.col; j++) {
-                const div = document.createElement("div")
-                div.setAttribute('lin', i)
-                div.setAttribute('col', j)
-                div.style.left = j * Logic.size + 'px'
-                div.style.top = i * Logic.size + 'px'
-                div.style.width = Logic.size + 'px'
-                div.style.height = Logic.size + 'px'
-                div.style.transition = '0.1s'
-                // div.innerHTML = '1'
-                div.innerHTML = Logic.Fruits[Logic.table[i][j]]
-                div.addEventListener('click', e => {
+    // Pieces() {
+    //     HTML.GameScreen.innerHTML = ''
+    //     for (let i = 0; i < Logic.lin; i++) {
+    //         for (let j = 0; j < Logic.col; j++) {
+    //             const div = document.createElement("div")
+    //             div.setAttribute('lin', i)
+    //             div.setAttribute('col', j)
+    //             div.style.left = j * Logic.size + 'px'
+    //             div.style.top = i * Logic.size + 'px'
+    //             div.style.width = Logic.size + 'px'
+    //             div.style.height = Logic.size + 'px'
 
-                    if (!Logic.blocks.includes(div)) {
-                        Logic.blocks.push(div)
-                    }
+                
 
-                    if (Logic.blocks.length == 2) {
-                        const l1 = Logic.blocks[0].getAttribute('lin')|0
-                        const c1 = Logic.blocks[0].getAttribute('col')|0
+    //             div.style.transition = '0.5s'
+    //             // div.innerHTML = '1'
+    //             // div.innerHTML = Logic.Fruits[Logic.table[i][j]]
+    //             div.innerHTML = Logic.Fruits[Logic.table[i][j]]
 
-                        const l2 = Logic.blocks[1].getAttribute('lin')|0
-                        const c2 = Logic.blocks[1].getAttribute('col')|0
+    //             div.addEventListener('click', e => {
 
-                        Logic.Change([l1, c1], [l2, c2])
+    //                 if (!Logic.blocks.includes(div)) {
+    //                     Logic.blocks.push(div)
+    //                 }
+
+    //                 if (Logic.blocks.length == 2) {
+    //                     const l1 = Logic.blocks[0].getAttribute('lin')|0
+    //                     const c1 = Logic.blocks[0].getAttribute('col')|0
+
+    //                     const l2 = Logic.blocks[1].getAttribute('lin')|0
+    //                     const c2 = Logic.blocks[1].getAttribute('col')|0
+
+    //                     Logic.Change([l1, c1], [l2, c2])
                         
-                        Logic.blocks = []
+    //                     Logic.blocks = []
 
-                        Logic.Falls()
-                    }
-
+    //                     // Logic.Falls()
+    //                     // Logic.Downs()
+    //                 }
                     
-                })
-                HTML.GameScreen.append(div)
+    //             })
+                    
+    //             HTML.GameScreen.append(div)
+
+                
+
+    //         }
+    //     }
+    // },
+
+    PieceAdd(cordinate, val=(Math.random() * Logic.randomness|0)){
+
+        const [i,j] = cordinate
+
+        if(val == -1) return
+
+        const div = document.createElement("div")
+        div.setAttribute('lin', i)
+        div.setAttribute('col', j)
+        div.style.left = j * Logic.size + 'px'
+        div.style.top = i * Logic.size + 'px'
+        div.style.width = Logic.size + 'px'
+        div.style.height = Logic.size + 'px'
+        div.style.transition = '0.2s'
+
+        div.style.opacity = 0
+        div.style.translate = '0px -'+Logic.size+'px'
+
+        div.innerHTML = Logic.Fruits[val]
+
+        div.addEventListener('click', e => {
+
+            if (!Logic.blocks.includes(div)) {
+                Logic.blocks.push(div)
             }
-        }
+
+            if (Logic.blocks.length == 2) {
+                const l1 = Logic.blocks[0].getAttribute('lin')|0
+                const c1 = Logic.blocks[0].getAttribute('col')|0
+
+                const l2 = Logic.blocks[1].getAttribute('lin')|0
+                const c2 = Logic.blocks[1].getAttribute('col')|0
+
+                Logic.Change([l1, c1], [l2, c2])
+                
+                Logic.blocks = []
+
+                // Logic.Falls()
+                // for(let r=0;r<Logic.col;r++){
+                //     Graphics.ClearLines()
+                // }
+                // Logic.Downs()
+                // setTimeout(()=>{
+                //     for(let t=0;t<Logic.lin;t++){
+                //         Logic.Downs()
+                //     }
+                // }, 300)
+                
+            }
+            
+        })
+
+        HTML.GameScreen.append(div)
+        Logic.table[i][j] = val
+
+        setTimeout(()=>{
+            div.style.opacity = 1
+            div.style.removeProperty('translate')
+        }, 0)
     },
 
     Change: (el1, el2) => {
@@ -260,7 +433,7 @@ const Graphics = {
         el2.setAttribute('col', c1)
     },
 
-    CheckBin(){
+    ClearLines(){
         const matrixB = Logic.SubMatrix()
         
         for(let i=0;i<Logic.lin;i++){
@@ -272,35 +445,68 @@ const Graphics = {
                         el.style.backgroundColor='#341a21'
                         el.style.opacity=0
                         
-                        setTimeout(()=>{
-                            el.remove()
-                        }, 1000)
+                        // setTimeout(()=>{
+                        //     el.remove()
+                        // }, 200)
+                        
+                        // setTimeout(()=>{
+                        //     // el.remove()
+                        //     for(let r=0;r<Logic.col;r++){
+                        //         Logic.Downs()
+                        //     }
+                        // }, 400)
+                        
+                        // setTimeout(()=>{
+                        //     Logic.Renew()
+                        // }, 500)
+
                     }catch(err){}
                 }else{
                     // el.style.backgroundColor
-                    setTimeout(()=> el.style.transition = '0.7s', 0 )
+                    setTimeout(()=> el.style.transition = '0.2s', 0 )
                     setTimeout(()=> el.style.transition = '0.2s', 700 )
                     el.style.backgroundColor='transparent'
                     
                 }
             }   
         }
+        // for(i=0;i<Logic.lin;i++){
+            // Graphics.ClearLines()
+            // Logic.Downs()
+        // }
     }
 }
 
 // Logic.setGrid(5, 4)
-Logic.setGrid(6, 4)
+Logic.setGrid(5, 4)
 Logic.generate()
 
+
 // Logic.generateTable([
-//     [0,0,0,1],
-//     [1,5,3,4],
-//     [4,5,5,5],
-//     [4,5,3,5],
-//     [4,5,3,5],
+//     [-1, -1, -1, 2],
+//     [0, 1, 2, 0],
+//     [4, 0, 3, 5],
+//     [2, 2, 1, 2],
+//     [1, 0, 1, 3],
 // ])
 
-// Graphics.CheckBin() //checa colunas
+// Graphics.ClearLines()
+// Graphics.ClearLines()
+// Graphics.ClearLines()
+// Graphics.ClearLines()
+// Graphics.ClearLines()
+
+// setTimeout(()=>{
+//     Logic.Downs()
+//     Logic.Downs()
+//     Logic.Downs()
+//     Logic.Downs()
+//     Logic.Downs()
+//     Logic.Downs()
+// }, 1000)
+
+
+// Graphics.ClearLines() //checa colunas
 
 
 function SubMatrix(){
@@ -324,124 +530,80 @@ function show(tab){
     }
 }
 
-sm = Logic.SubMatrix()
+// sm = Logic.SubMatrix()
 
 window.onkeyup = (e) => {
-    if(e.key == 1) {
-        // sm = Logic.SubMatrix()
-        // console.log(sm)
-        show(sm)
-        Graphics.CheckBin()
+    if(e.key == '1') {
+        Graphics.ClearLines()
+        // for(i=0;i<Logic.lin;i++)
+        // Logic.Downs()
+    }
+    if(e.key == '2') {
+        // Graphics.ClearLines()
+        for(i=0;i<Logic.lin;i++)
+            Logic.Downs()
+    }
+    if(e.key == '3') {
+        Logic.Renew()
     }
 
-    if(e.key == 2) {
-        for(i=0;i<Logic.lin;i++){
-            for(j=0;j<Logic.col;j++){
-                if(sm[i][j] == -1){
-                    try{
-                        if(sm[i][j] == -1){
-                            aux = sm[i][j]
-                            sm[i][j] = sm[i-1][j]
-                            sm[i-1][j] = aux
-                            Logic.Swap([i,j],[i-1,j])
+}
+
+setInterval(()=>{
+    const matrixB = Logic.SubMatrix()
+    for(let i=0;i<Logic.lin;i++){
+        for(let j=0;j<Logic.col;j++){
+            const el = Graphics.getElementByCoordinate(i,j)
+            if(matrixB[i][j] == -1){
+                
+                try{
+                    el.style.backgroundColor='#341a21'
+                    el.style.opacity=0
+                    
+                    setTimeout(()=>{
+                        el.remove()
+                    }, 200)
+                    
+                    setTimeout(()=>{
+                        el.remove()
+                        for(let r=0;r<Logic.col;r++){
+                            Logic.Downs()
                         }
-                    }catch(err){}
-                }
-            }
-        }
-        console.log('--')
-        show(sm)
-        Logic.table = sm
-    }
+                    }, 500)
 
-    if(e.key == 3){
-        Logic.Falls()
-    }
-    // Logic.Change([2, 3], [4, 4])
-    // Logic.Change([0, 0], [2, 3])
+                    let test = false
+                    Logic.SubMatrix().map(e=> {
+                        if(e.includes(-1)){
+                            test = true
+                        }
+                    } )
+                    
+                    if(test){
+                        setTimeout(()=>{
+                            Logic.Renew()
+                        }, 700)
+                    }
+
+            // Graphics.ClearLines()
+            // Logic.Downs()
+            }catch(err){}
+        }}}
+}, 300)
+
+
+
+const HTMLEvents = {
+    col: {
+        add(){ Logic.setGrid(Logic.lin, ++Logic.col) },
+        sub(){ Logic.setGrid(Logic.lin, --Logic.col) },
+    },
+    lin: {
+        add(){ Logic.setGrid(++Logic.lin, Logic.col) },
+        sub(){ Logic.setGrid(--Logic.lin, Logic.col) },
+    },
+    rand: {
+        add(){ ++Logic.randomness },
+        sub(){ --Logic.randomness },
+    },
+    
 }
-
-function fallLine(a, b, k){
-    // a = [1,0,1,1,0]
-    // b = [0,0,0,1,1]
-    for(let li=0;li<a.length;li++){
-        if(a[li] < b[li]){
-                b[li] = 1
-                a[li] = 0
-        }
-    }
-    return {
-        a: a,
-        b: b
-    }
-}
-
-
-// function go(){
-//     SumMatrix = Logic.SumMatrix()
-//     SumMatrix2 = []
-
-//     console.log(SumMatrix)
-
-//     for(i=SumMatrix.length-1;i>=0;i--){
-
-//         SumMatrix2[i] = []
-
-//         const pa = SumMatrix[i]
-//         const pb = SumMatrix[i-1]
-        
-        
-//         // console.log(pb)
-        
-//         // console.log(pa)
-//         for(j=0;j<SumMatrix[i].length;j++){
-//             if(i>0){
-//                 if(pb[j] < pa[j]){
-//                     pa[j] = 0
-//                     pb[j] = 1
-//                 }
-                
-                
-//                 // SumMatrix2[i][j] = 1
-//                 // Graphics.Change()
-//                 // else{
-//                     //     SumMatrix2[i][j] = pb[j]
-//                     // }
-//                     // SumMatrix2
-//                     SumMatrix2[i] = pb
-//                     SumMatrix2[i-1] = pa
-//             }
-//             // else{
-//             //     a = 1
-//             //     if(SumMatrix[i][j] > SumMatrix[i+1][j]){
-//             //         a = 0
-//             //     }
-//             //     SumMatrix2[i][j] = a
-//             // }
-//             // try{
-//                 // if(pb[j] > pa[j]){
-//                     // console.log(SumMatrix[i-1])
-//                     // console.log(SumMatrix[i])
-//                     // SumMatrix[i][j] = 1
-//                     // SumMatrix[i-1][j] = 0
-
-//                     // SumMatrix2[i][j] = 1
-//                     // SumMatrix2[i-1][j] = 0
-
-//                     // console.log(SumMatrix[i-1])
-//                     // console.log(SumMatrix[i])
-//                     // console.log(`linha ${i} : ${SumMatrix[i]}`)
-//                     // console.log('---')
-//                 // }else{
-//                 //     SumMatrix2[i][j] = SumMatrix[i][j]
-//                 // }
-//             // }catch(e){}
-//         }
-//     }
-//     console.log(SumMatrix2)
-
-// }
-
-
-
-
